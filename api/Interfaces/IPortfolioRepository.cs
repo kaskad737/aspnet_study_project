d835db892bc20041927ed7257e82cde98ead2989
Apti <aptia@thinkeasy.cz>
2024-08-11 16:21:18 +0200

    add portfolio get

---
+++ api/Interfaces/IPortfolioRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Models;
+using Models;
+
+namespace api.Interfaces
+{
+    public interface IPortfolioRepository
+    {
+        Task<List<Stock>> GetUserPortfolio(AppUser user);
+    }
+}
