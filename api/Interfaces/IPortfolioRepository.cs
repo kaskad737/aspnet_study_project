72d2eb026cfefd47a3f52810ce700db60e3def7f
Apti <aptia@thinkeasy.cz>
2024-08-12 20:55:45 +0200

    add portfolio delete

--- api/Interfaces/IPortfolioRepository.cs
+++ api/Interfaces/IPortfolioRepository.cs
@@ -11,5 +11,6 @@ namespace api.Interfaces
     {
         Task<List<Stock>> GetUserPortfolio(AppUser user);
         Task<Portfolio> CreateAsync(Portfolio portfolio);
+        Task<Portfolio?> DeletePortfolio(AppUser appUser, string symbol);
     }
 }
